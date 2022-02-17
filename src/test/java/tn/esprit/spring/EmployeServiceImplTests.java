d3e85e632197097a4adcd246488ef0e09cbed4ef
khenissialadin <khenissialadin952@gmail.com>
2022-02-17 15:50:29 +0100

    added employe tests

--- src/test/java/tn/esprit/spring/EmployeServiceImplTests.java
+++ src/test/java/tn/esprit/spring/EmployeServiceImplTests.java
@@ -5,6 +5,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
 import org.junit.Assert;
 
 import tn.esprit.spring.entities.Employe;
@@ -25,10 +28,22 @@ public class EmployeServiceImplTests {
 		employe.setNom("aaaa");
 		employe.setPrenom("sfsfe");
 		int id = employeService.ajouterEmploye(employe);
-		Assert.assertNotEquals(id, 0);
+		Assert.assertEquals(employeService.getEmployePrenomById(id), employe.getPrenom());
 		employeService.deleteEmployeById(id);
 		String prenom = employeService.getEmployePrenomById(id);
 		Assert.assertNull(prenom);
+	}
+	
+	@Test
+	public void getAllEmployeNamesJPQLTest() {
+		List<String> names = employeService.getAllEmployeNamesJPQL();
+		Assert.assertNotNull(names);
+	}
+	
+	@Test
+	public void getAllEmployesTest() {
+		List<Employe> emps = employeService.getAllEmployes();
+		Assert.assertNotNull(emps);
 	}
 
 
