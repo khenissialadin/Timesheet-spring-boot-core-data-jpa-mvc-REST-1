d3d49bed99053eb6c135c6de64b897edf8fab478
mkyahyaoui <73721548+mkyahyaoui@users.noreply.github.com>
2022-02-22 20:06:08 +0100

    update EmployeServiceImplTest

--- src/test/java/tn/esprit/spring/EmployeServiceImplTests.java
+++ src/test/java/tn/esprit/spring/EmployeServiceImplTests.java
@@ -1,9 +1,14 @@
 package tn.esprit.spring;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+import java.util.ArrayList;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -18,11 +23,13 @@ import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.entities.Mission;
 import tn.esprit.spring.entities.Timesheet;
 import tn.esprit.spring.entities.TimesheetPK;
+import tn.esprit.spring.entities.Entreprise;
 import tn.esprit.spring.repository.ContratRepository;
 import tn.esprit.spring.repository.MissionRepository;
 import tn.esprit.spring.services.IEmployeService;
 import tn.esprit.spring.services.IEntrepriseService;
 import tn.esprit.spring.services.ITimesheetService;
+import tn.esprit.spring.repository.EmployeRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -42,7 +49,10 @@ public class EmployeServiceImplTests {
 
 	@Autowired
 	MissionRepository missionRepository;
-
+	
+	@Autowired
+	EmployeRepository employeRepository;
+	
 	@Test
 	public void ajouterEmployeTest() {
 		Employe employe = new Employe();
@@ -170,5 +180,134 @@ public class EmployeServiceImplTests {
 		Employe assetEmp = employeService.findEmployeById(emp.getId());
 		Assert.assertEquals(assetEmp.getEmail(), email);
 		employeService.deleteEmployeById(emp.getId());
+	}
+	
+	@Test
+	public void affecterContractEmployeTest() {
+		Employe employe= new Employe();
+		employe.setActif(false);
+		employe.setEmail("efsffs");
+		employe.setNom("aaaa");
+		employe.setPrenom("sfsfe");
+		
+		Contrat contrat = new Contrat();
+		contrat.setReference(new Random().nextInt(999));
+		contrat.setDateDebut(null);
+		contrat.setEmploye(employe);
+		contrat.setSalaire(100);
+		contrat.setTypeContrat(null);
+		
+		employeService.ajouterEmploye(employe);
+		//int ctrId = employeService.ajouterContrat(contrat);
+		
+		employeService.affecterContratAEmploye(contrat.getReference(), employe.getId());
+		Contrat ctr = employeService.getContratById(contrat.getReference());
+			
+		assertNull(ctr);
+	}
+
+	@Test
+	public void ajouterContratTest(){
+		Employe employe= new Employe();
+		employe.setActif(false);
+		employe.setEmail("efsffs");
+		employe.setNom("aaaa");
+		employe.setPrenom("sfsfe");
+		
+		Contrat contrat = new Contrat();
+		contrat.setReference(new Random().nextInt(999));
+		contrat.setDateDebut(null);
+		contrat.setEmploye(employe);
+		contrat.setSalaire(100);
+		contrat.setTypeContrat(null);
+		
+		employeService.ajouterEmploye(employe);
+		int idocntract = employeService.ajouterContrat(contrat);
+		Optional<Contrat> ctr = contratRepository.findById(idocntract);
+		if(ctr.isPresent()) {
+			assertEquals(idocntract, contrat.getReference());
+		}		
+	}
+	@Test
+	public void deleteContratByIdTest() {
+		Contrat contrat = new Contrat();
+		contrat.setReference(new Random().nextInt(999));
+		contrat.setDateDebut(null);
+		contrat.setEmploye(null);
+		contrat.setSalaire(100);
+		contrat.setTypeContrat(null);
+		
+		int idocntract = employeService.ajouterContrat(contrat);
+		
+		employeService.deleteContratById(idocntract);
+		Optional<Contrat> ctr = contratRepository.findById(contrat.getReference());
+		assertEquals(true, ctr.isEmpty());
+		
+	}
+	@Test
+	public void deleteEmployeByIdTest() {
+		Employe employe= new Employe();
+		employe.setId(new Random().nextInt(999));
+		employe.setActif(false);
+		employe.setEmail("efsffs");
+		employe.setNom("aaaa");
+		employe.setPrenom("sfsfe");
+		int empId = employeService.ajouterEmploye(employe);
+		
+		employeService.deleteEmployeById(empId);		
+		Optional<Employe> employesAfterDeleteAction = employeRepository.findById(employe.getId());
+		assertEquals(true, employesAfterDeleteAction.isEmpty());
+		
+	}
+	/*@Test
+	public void desaffecterEmployeDuDepartementTest() {
+		List<Employe> empls = new ArrayList<>();
+		Employe employe= new Employe();
+		employe.setId(new Random().nextInt(999));
+		employe.setActif(false);
+		employe.setEmail("efsffs");
+		employe.setNom("aaaa");
+		employe.setPrenom("sfsfe");
+		employeService.ajouterEmploye(employe);
+		empls.add(employe);
+		
+		Departement departement = new Departement();
+		departement.setId(101);
+		departement.setName(null);
+		departement.setEntreprise(null);
+		departement.setEmployes(empls);
+		entrepriseService.ajouterDepartement(departement);
+		
+		employeService.affecterEmployeADepartement(employe.getId(),departement.getId());
+		assertNotNull(departement.getEmployes());
+	
+		employeService.desaffecterEmployeDuDepartement(employe.getId(), departement.getId());
+		assertNull(departement.getEmployes());
+		
+	}*/
+	@Test
+	public void getAllEmployeByEntreprise() {
+		    Entreprise entreprise = new Entreprise();
+	        entreprise.setId(1);
+	        entreprise.setName("e1");
+	        entreprise.setRaisonSocial("s1");
+	        
+	        Employe employe = new Employe();
+	        employe.setNom("aaaa");
+	        employe.setId(01);
+	        
+	        List<Employe> employeList= new ArrayList<>();
+	        employeList.add(employe);
+	        
+	        List<Departement> departements= new ArrayList<>();
+	        Departement depart1 = new Departement();
+	        depart1.setEmployes(employeList);
+	        List<Departement> departList= new ArrayList<>();
+	        departList.add(depart1);
+	        entreprise.setDepartements(departList);
+	        
+	        List<Employe> employeNames = employeService.getAllEmployeByEntreprise(entreprise);
+	        assertNotNull(employeNames);
+		
 	}
 }
